df3d1d91f749da5231f810d40064b083abe6f638
Olga Suslova <ONSuslova@gmail.com>
2022-11-08 19:49:41 +0300

    Homework 6

---
+++ Introduction to programming languages/Homework 6/Task 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] GetArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++) array[i] = int.Parse(Console.ReadLine());
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("Вы ввели: ");
+    foreach (var el in arr) Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+
+int PositiveCount(int[] arr)
+{
+    int count = 0;
+    foreach (var el in arr) count += el > 0 ? 1 : 0;
+    return count;
+}
+
+void Main()
+{
+    Console.WriteLine($"Какое количество чисел Вы хотите ввести?");
+    int count = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Введите числа через Enter:");
+    int[] array = GetArray(count);
+    PrintArray(array);
+    Console.WriteLine($"Количество позитивных чисел равно {PositiveCount(array)}");
+}
+
+Main();
---
+++ Introduction to programming languages/Homework 6/Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+float[] ReadKoeff(int cnt)
+{
+    float[] koeff = new float[cnt];
+    Console.WriteLine($"Введите коэффициенты прямых (b1, k1), (b2, k2):");
+    string[] koeffName = new string[] {"b1", "k1", "b2", "k2"};
+    for (var i = 0; i < koeff.Length; i++) 
+        {
+            System.Console.Write($"{koeffName[i]}: ");
+            koeff[i] = float.Parse(Console.ReadLine());
+        }
+    return koeff;
+}
+
+(float, float) Solver(float[] koeff)
+{
+    float x = (koeff[2] - koeff[0]) / (koeff[1] - koeff[3]);
+    float y = koeff[1] * x + koeff[0];
+    return (x, y);
+}
+
+void Main()
+{
+    const int cnt = 4;
+    float[] koeff = new float[cnt];
+    koeff = ReadKoeff(cnt);
+    if (koeff[1] == koeff[3]) 
+        {
+            System.Console.WriteLine("Решения нет / много.");
+            return;
+        }
+    (float, float) decision = Solver(koeff);
+    System.Console.WriteLine($"Решение уравнения: ({decision.Item1}, {decision.Item2})");
+}
+
+Main();
