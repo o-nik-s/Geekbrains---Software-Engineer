6d8182eaa9df4e1019b12daa04d5ff5ee32ccc6f
Olga Suslova <ONSuslova@gmail.com>
2023-01-20 23:38:10 +0300

    Homework 5

---
+++ Introduction to Python/Homework 5/Task 2_b.py
@@ -0,0 +1,49 @@
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета.
+# Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой.
+# За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+import random
+
+
+def step_human(count, max_c):
+    count -= int(input(f'Ваш ход! Введите количество конфет (не более {min(max_c, count)}): '))
+    print(f'Осталось {count} конфет.')
+    return count
+
+
+def step_pc(count, max_c):
+    # cnt = random.randint(1, max_c)
+    cnt = (count - 1) % max_c if count > max_c else max_c
+    if cnt == 0: cnt = max_c
+    if cnt > count:
+        cnt = count
+    count -= cnt
+    print(f'Ход ПК. ПК забрал {cnt} конфет. Осталось {count} конфет.')
+    return count
+
+
+def main():
+    count = 2021
+    max_c = 28
+
+    print(f'Всего {count} конфет.')
+    flag = bool(random.randint(0, 1))
+    while count > 0:
+        if flag:
+            count = step_human(count, max_c)
+        else:
+            count = step_pc(count, max_c)
+        flag = not flag
+    if flag:
+        print(f'Победил ПК!')
+    else:
+        print(f'Поздравляем с победой!')
+
+
+main()
