58dba50ae32aed0280b75d10c39f6955e13af93b
Olga Suslova <ONSuslova@gmail.com>
2023-01-27 22:21:01 +0300

    Homework 7

--- Introduction to Python/Homework 7/export_data.py
+++ Introduction to Python/Homework 7/export_data.py
@@ -8,5 +8,6 @@ def export_data(data:list=None, file_name:str='', encoding:str='UTF-8'):
     if format is None: return
     with open(file_name, 'w', encoding=encoding) as file:
         data = prepare_data(data, format)
-        file.writelines(data)
+        if format in ['csv', 'txt']: file.writelines(data)
+        elif format == 'xml': file.write(data)
     print('Экспорт данных выполнен!')
--- Introduction to Python/Homework 7/import_data.py
+++ Introduction to Python/Homework 7/import_data.py
@@ -1,12 +1,28 @@
 from prepare_data import *
 
+
+def import_data_from_xml(text:str=None):
+    data = text.strip().split('\n')
+    lst, person_lst = list(), list()
+    for info in map(lambda x: x.strip(), data):
+        # print(info)
+        if info == '<person>':
+            person_lst = list()
+        elif info == '</person>':
+            lst.append(person_lst)
+        else: 
+            person_lst.append(info[info.find('>')+1:info.find('</')])
+    return lst
+    
+
 def import_data(file_name:str, encoding:str='UTF-8'):
     format = define_format(file_name)
     if format is None: return
     with open(file_name, 'r', encoding=encoding) as file:
         text = file.read()
-    if format == 'csv': text = list(map(lambda x: x.split(';'), text.split('\n')))
-    elif format == 'txt': text = list(map(lambda x: x.split('\n'), text.split('\n\n')))
-    while text[-1] == ['']: text = text[:-1]
+    if format == 'csv': lst = list(map(lambda x: x.split(';'), text.split('\n')))
+    elif format == 'txt': lst = list(map(lambda x: x.split('\n'), text.split('\n\n')))
+    elif format == 'xml': lst = import_data_from_xml(text)
+    while lst[-1] == ['']: lst = lst[:-1]
     print('Импорт данных выполнен!')
-    return text
+    return lst
