26c13b49036bb74a8fb5153e231830d942157f8d
Olga Suslova <ONSuslova@gmail.com>
2023-03-16 19:30:07 +0300

    Homework 5

--- Introduction to Java/Homework_5/Task_2_2.java
+++ Introduction to Java/Homework_5/Task_2_2.java
@@ -3,18 +3,26 @@
 
 package geekbrains_course.Homework_5;
 
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Task_2 {
+public class Task_2_2 {
+
+    static String fileName = "text.txt"; // Данный файл должен лежать в корневой директории проекта и быть с размеченными переносами строк
 
     public static void main(String[] args) throws Exception {
 
+        String dir = System.getProperty("user.dir");
+
+        String text = readTextFile(dir + "\\" + fileName);
+//        System.out.println(text);
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите текст: ");
-        String text = scanner.nextLine();
+//        System.out.println("Введите текст: ");
+//        String text = scanner.nextLine();
         System.out.print("Введите искомое слово: ");
         String searchWord = scanner.nextLine();
         scanner.close();
@@ -36,6 +44,11 @@ public class Task_2 {
         count = count == null ? 0 : count;
         System.out.println("Слово " + searchWord + " встретилось " + count  + " раз");
 
+    }
+
+    public static String readTextFile(String fileName) throws Exception {
+        String text = Files.lines(Paths.get(fileName)).reduce("", String::concat);
+        return text;
     }
 
 }
