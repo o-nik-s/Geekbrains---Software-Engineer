f81e5ad424cf41106b573842c025dd33950b708c
Olga Suslova <ONSuslova@gmail.com>
2023-06-08 21:08:06 +0300

    Homework 3

--- Databases and SQL/Homework 3/Create tables.sql
+++ Databases and SQL/Homework 3/Create tables.sql
@@ -18,10 +18,29 @@ CREATE TABLE IF NOT EXISTS Customers
     Rating INT,
     SNum INT);
 
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2001', 'Hoffman', 'London', 100, 1001);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2002', 'Giovanni', 'Rome', 200, 1003);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2003', 'Liu', 'SanJose', 200, 1002);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2004', 'Grass', 'Berlin', 300, 1002);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2006', 'Clemens', 'London', 100, 1001);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2008', 'Cisneros', 'SanJose', 300, 1007);
-INSERT INTO `db_geekbrains`.`SalesPeople` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2007', 'Pereira', 'Rome', 100, 1004);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2001', 'Hoffman', 'London', 100, 1001);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2002', 'Giovanni', 'Rome', 200, 1003);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2003', 'Liu', 'SanJose', 200, 1002);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2004', 'Grass', 'Berlin', 300, 1002);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2006', 'Clemens', 'London', 100, 1001);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2008', 'Cisneros', 'SanJose', 300, 1007);
+INSERT INTO `db_geekbrains`.`Customers` (`CNum`, `CName`, `City`, `Rating`, `SNum`) VALUES ('2007', 'Pereira', 'Rome', 100, 1004);
+
+
+CREATE TABLE IF NOT EXISTS Orders
+	(ONum SERIAL PRIMARY KEY,
+    Amt DECIMAL(9,2),
+    ODate DATE NOT NULL,
+    CNum INT NOT NULL,
+    SNum INT);
+
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3001', 18.69, '1990/03/10', 2008, 1007);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3003', 767.19, '1990/03/10', 2001, 1001);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3002', 1900.10, '1990/03/10', 2007, 1004);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3005', 5160.15, '1990/03/10', 2003, 1002);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3006', 1098.16, '1990/03/10', 2008, 1007);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3009', 1713.23, '1990/04/10', 2002, 1003);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3007', 75.75, '1990/04/10', 2004, 1002);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3008', 4723.00, '1990/05/10', 2006, 1001);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3010', 1309.95, '1990/06/10', 2004, 1002);
+INSERT INTO `db_geekbrains`.`Orders` (`ONum`, `Amt`, `ODate`, `CNum`, `SNum`) VALUES ('3011', 9891.88, '1990/06/10', 2006, 1001);
---
+++ Databases and SQL/Homework 3/Part 1.sql
@@ -0,0 +1,24 @@
+-- 1. Напишите запрос, который вывел бы таблицу со столбцами в следующем порядке: city, sname, snum, comm (к первой или второй таблице, используя SELECT).
+Select City, SName, SNum, Comm from SalesPeople
+
+-- 2. Напишите команду SELECT, которая вывела бы оценку (rating), сопровождаемую именем каждого заказчика в городе San Jose (“заказчики”).
+Select Cname, Rating from Customers
+where City = 'SanJose'
+
+-- 3. Напишите запрос, который вывел бы значения snum всех продавцов из таблицы заказов без каких бы то ни было повторений (уникальные значения в “snum“ “Продавцы”)
+Select distinct SNum from Customers
+
+-- 4. Напишите запрос, который бы выбирал заказчиков, чьи имена начинаются с буквы G. Используется оператор "LIKE": (“заказчики”) 
+-- https://dev.mysql.com/doc/refman/8.0/en/string-comparison-functions.html
+Select Cname, Rating from Customers
+where Cname like "G%"
+
+-- 5. Напишите запрос, который может дать вам все заказы со значениями суммы выше чем $1,000 (“Заказы”, “amt” - сумма).
+Select ONum, Amt, ODate from Orders where Amt>'1000'
+
+-- 6. Напишите запрос который выбрал бы наименьшую сумму заказа. (Из поля “amt” - сумма в таблице “Заказы” выбрать наименьшее значение).
+Select min(Amt) as Amt from Orders
+
+-- 7. Напишите запрос к таблице “Заказчики”, который может показать всех заказчиков, у которых рейтинг больше 100 и они находятся не в Риме.
+Select CName, City, Rating from Customers
+where Rating > 100 and City != 'Rome'
