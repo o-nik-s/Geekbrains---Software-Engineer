80e051d640ab4a8e1111e70cfe77764fbb4e29a7
Olga Suslova <ONSuslova@gmail.com>
2023-06-20 15:13:33 +0300

    Homework 6

---
+++ Databases and SQL/Homework 6/Task 2.sql
@@ -0,0 +1,15 @@
+-- 2. Выведите только четные числа от 1 до 10. Пример: 2,4,6,8,10 
+Drop procedure if exists print_even;
+DELIMITER // 
+CREATE PROCEDURE print_even (IN max_value INT) 
+BEGIN 
+    DECLARE i INT DEFAULT 1;
+    DECLARE res VARCHAR(100) DEFAULT "";
+    WHILE i <= max_value DO 
+		IF mod(i, 2) = 0 then SET res = CONCAT(res, ' ', i); end if; 
+        SET i = i + 1; 
+    END WHILE;
+    SELECT res;
+END // 
+
+Call print_even(10);
