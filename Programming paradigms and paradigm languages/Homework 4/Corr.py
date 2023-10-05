e6250dc39cabc049edcfeb0a77a870ff84930db3
Olga Suslova <ONSuslova@gmail.com>
2023-10-05 10:03:06 +0300

    Homework 4

--- Programming paradigms and paradigm languages/Homework 4/Corr.py
+++ Programming paradigms and paradigm languages/Homework 4/Corr.py
@@ -1,14 +1,12 @@
 import random
 
-cnt = 10
-mn = -10
-mx = 10
+cnt, mn, mx = 10, -10, 10
 
-X = [random.randint(-10, 10) for _ in range(cnt)]
-Y = [random.randint(-10, 10) for _ in range(cnt)]
+X = [random.randint(mn, mx) for _ in range(cnt)]
+Y = [random.randint(mn, mx) for _ in range(cnt)]
 
 Mx, My, ln = sum(X)/len(X), sum(Y)/len(Y), len(X)
 
-r = sum((X[i] - Mx)*(Y[i] - My) for i in range(len(X))) / (sum((X[i] - Mx)**2 for i in range(len(X))) * sum((Y[i] - My)**2 for i in range(len(Y))))**(1/2)
+r = sum((X[i] - Mx)*(Y[i] - My) for i in range(ln)) / (sum((X[i] - Mx)**2 for i in range(ln)) * sum((Y[i] - My)**2 for i in range(ln)))**(1/2)
 
 print(f'Количество элементов: {cnt}, X={X}, Y={Y}', f'r={r}', sep='\n')
