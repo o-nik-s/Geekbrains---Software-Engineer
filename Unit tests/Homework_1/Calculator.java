98cca09abe7b135eb876b55b69e90045964ae17e
Olga Suslova <ONSuslova@gmail.com>
2023-10-11 03:39:06 +0300

    Homework_1

---
+++ Unit tests/Homework_1/Calculator.java
@@ -0,0 +1,21 @@
+package Homework1;
+
+/**
+ * В классе Calculator создайте метод calculateDiscount, который принимает сумму покупки и процент скидки
+ * и возвращает сумму с учетом скидки. Ваша задача - проверить этот метод с использованием библиотеки AssertJ.
+ * Если метод calculateDiscount получает недопустимые аргументы, он должен выбрасывать исключение ArithmeticException.
+ * Не забудьте написать тесты для проверки этого поведения.
+ */
+public class Calculator {
+    public static double calculateDiscount(double purchaseAmount, int discountPercent) {
+        if (purchaseAmount < 0 || discountPercent < 0 || discountPercent > 100) {
+            throw new ArithmeticException("Некорректные аргументы");
+        }
+        return purchaseAmount - purchaseAmount * discountPercent / 100;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateDiscount(1000,20));
+    }
+
+}
---
+++ Unit tests/Homework_1/CalculatorTest.java
@@ -0,0 +1,33 @@
+package Homework1;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculatorTest {
+
+    @Test
+    public void testCalculateDiscount() {
+        Calculator calculator = new Calculator();
+
+        double result = calculator.calculateDiscount(100, 10);
+        assertThat(result).isEqualTo(90);
+
+        result = calculator.calculateDiscount(50, 5);
+        assertThat(result).isEqualTo(47.5);
+
+        assertEquals(82, calculator.calculateDiscount(100, 20), "Ошибка в вычислениях");
+        assertTrue(calculator.calculateDiscount(50, 10) == 45, "Ошибка в вычислениях");
+
+    }
+
+
+    @Test
+    public void testCalculateDiscountWithInvalidArguments() {
+        Calculator calculator = new Calculator();
+
+        calculator.calculateDiscount(-100, 10);
+    }
+
+}
